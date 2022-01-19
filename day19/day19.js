3dd5ca5c87a562940925b7ddb8a7c067931fa146
whichlight <whichlight@gmail.com>
2022-01-19 18:12:02 -0500

    day19

---
+++ day19/day19.js
@@ -0,0 +1,53 @@
+/**
+ * genuary day 18
+ * Prompt: VHS
+ * 
+ * <3 whichlight 
+ * 
+ * 
+ * Prompt: Use text/typography
+ * 
+ */
+
+let w, h = 0;
+const mantra = "i love you. all of you. you are radiant, you are enough. ";
+let index = 0; 
+
+
+function setup() {
+    w = windowWidth;
+    h = windowHeight;
+    frameRate(10);
+    select('canvas').remove();
+}
+
+function draw() {
+    frameRate(10);   
+
+    let m = select('#mantra');
+
+    let w = mantra[index]; 
+    m.html(w, true);
+    
+    if(w=="." || w ==","){
+        m.html("<br>", true);
+        frameRate(1);
+
+    }
+
+    index++; 
+    index%=mantra.length; 
+
+    if(m.height > h){
+        console.log(m.height);
+        m.html('');
+        index = 0; 
+
+        let t = select('#title');
+        t.html(" <3 ",true);
+
+    }
+    
+
+}   
+
