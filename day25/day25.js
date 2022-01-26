04970edcb00b40b662dc85dca85b007d39258844
whichlight <whichlight@gmail.com>
2022-01-25 22:27:38 -0500

    mobile touch events

--- day25/day25.js
+++ day25/day25.js
@@ -6,89 +6,52 @@
  * 
  */
 
- let w,h=0;
- let side = 100;
- let boxes = [];
- let level =0; 
- let gcol = 300; 
+let w, h = 0;
+let side = 100;
+let level = 0;
+let gcol = 300;
+let boxes = [];
 
-
-
-function setup() {
+setup = () => {
     h = windowHeight;
     w = windowWidth;
-    side = floor(min(w,h)/7);
+    side = floor(min(w, h) / 7);
     colorMode(HSB, 360, 100, 100, 100);
     createCanvas(w, h, WEBGL);
     frameRate(20);
-    render({x: w/2, y: h/2, c: 300}, 0); 
-
+    render({ x: w / 2, y: h / 2, c: 300 }, 0);
 }
 
-function draw() {
-    background(300,100,100,100)
-
-    camera(0,0,level*side+500);
+draw = () => {
+    background(300, 100, 100, 100)
+    camera(0, 0, level * side + 500);
     
-    if(boxes.length==0){
-        render({x: 0, y: 0, c: 300}, level*side); 
-    }
+    if (boxes.length == 0) render({ x: 0, y: 0, c: 300 }, level * side);
 
-    boxes.forEach(function(p){
-        render({x: p.x, y:p.y, c: p.c}, side*p.z);
-    })
+    boxes.forEach((p) => render({ x: p.x, y: p.y, c: p.c }, side * p.z));
 
-    if(!mouseIsPressed && level>0){
-        //boxes.pop(); 
-        level--; 
+    if (!mouseIsPressed && level > 0) {
+        level--;
         gcol = random(360);
-
     }
 
-    if(mouseIsPressed){
-        let b = {x: mouseX-w/2, y: mouseY-h/2, c: gcol, z: level};
-    boxes.push(b)
-    level++; 
-    }
-    
-
-}
-
-
-/*
-
-function mouseDragged(){
-    let b = {x: mouseX-w/2, y: mouseY-h/2, c: gcol, z: level};
-    boxes.push(b)
-    level++; 
-
-
-    // this section will limit adding a box if it wasn't in a new bin 
-     if(boxes.length==0){
+    if (mouseIsPressed) {
+        let b = { x: mouseX - w / 2, y: mouseY - h / 2, c: gcol, z: level };
         boxes.push(b)
-    } 
-
-    else {
-        let xval = floor((mouseX-w/2)/side);
-        let yval = floor((mouseY-h/2)/side);
-        let xold = floor((boxes[boxes.length-1].x)/side);
-        let yold = floor((boxes[boxes.length-1].y)/side);
-
-        if(xval!=xold || yval!=yold){
-            boxes.push(b);
- 
-        }
+        level++;
     }
-   
-}
- */
 
-function render(p,i){
+}
+
+touchStarted = () => false;
+
+let render = (p, i) => {
     push();
-    translate(p.x, p.y ,i);
+    translate(p.x, p.y, i);
     strokeWeight(3);
-    stroke(60,100,100);
-    fill(p.c,100,100, 50);
+    stroke(60, 100, 100);
+    fill(p.c, 100, 100, 50);
     box(side);
     pop();
 }
+
