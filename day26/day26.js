14902a2dae97b1c3ce3f32114d1d6376c669a4de
whichlight <whichlight@gmail.com>
2022-01-26 23:30:01 -0500

    day26

---
+++ day26/day26.js
@@ -0,0 +1,68 @@
+/**
+ * genuary day 26
+ * Prompt: airport carpet
+ * 
+ * <3 whichlight 
+ * 
+ */
+
+let w, h = 0;
+let numSideTiles = 4;
+let side = 100; 
+let tile = []; //side x side tile 
+let bin = 10; 
+let tileLen; 
+let gcol = 300; 
+let cols = [0, 30, 60, 120, 210, 240, 270, 300];
+
+setup = () => {
+    h = windowHeight;
+    w = windowWidth;
+    side = floor(min(w,h)/numSideTiles); 
+    colorMode(HSB, 360, 100, 100, 100);
+    createCanvas(w, h);
+
+    //initialize 
+     tileLen = floor(side/bin); 
+  
+    for(let i = 0; i<tileLen; i++){
+        let tmp = []; 
+        for(let i = 0; i<tileLen; i++){
+            tmp.push(0); 
+        }
+        tile.push(tmp); 
+    }
+    background(180, 100, 100, 100)
+
+}
+
+draw = () => {
+
+    if(mouseIsPressed){
+        let i = floor((mouseX%side)/bin); 
+        let j = floor((mouseY%side)/bin); 
+        
+        if(i<tile.length && j<tile.length){
+            tile[i][j]= gcol; 
+            drawTilePixel(i,j,gcol);
+        }
+    }
+
+    if(!mouseIsPressed){
+        gcol = randColor(); 
+    }
+}
+
+const drawTilePixel = (i,j,gcol) => {
+    for(let x = 0; x<w/side; x++){
+        for(let y = 0; y<h/side; y++){
+            noStroke();
+            fill(gcol, 100, 100);
+            rect((side*x+i*bin), (side*y+j*bin), bin, bin);
+        }
+    }
+}
+
+const randColor = () => cols[floor(random(cols.length))];
+
+
